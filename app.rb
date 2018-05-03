1d66d5a881f21c0c7b2d9dc077c7c4c972f76caf
kevin <khesse92@gmail.com>
2018-05-03 06:53:47 -0400

    dirrected sign in page , not fully functional at this time

--- app.rb
+++ app.rb
@@ -8,11 +8,25 @@ set :database, "sqlite3:app.db"
 
 get '/' do
   erb :home
+  # add 2 homepage for signed in or signed out
 end
 
 get '/sign_in' do 
   erb :sign_in
 end
+
+post '/sign-in' do 
+  @user = User.find_by(username: params[:username])
+
+  if @user && @user.password == params[:password]
+    session[:user_id] = @user.id
+    redirect '/'
+
+  else 
+    redirect '/sign-up'
+  end
+end
+
 
 get '/sign_up' do 
   erb :sign_up
