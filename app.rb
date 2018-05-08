80fb5393b073d228e7ab0c9f262aff7b5dbffa6e
kevin <khesse92@gmail.com>
2018-05-08 16:38:31 -0400

    cleaned up code

--- app.rb
+++ app.rb
@@ -1,6 +1,5 @@
 require "sinatra"
 require "sinatra/activerecord"
-# require "sinatra/flash"
 require "./models"
 require "sinatra/content_for"
 
@@ -16,18 +15,14 @@ set :sessions, true
 
 
 get '/' do
-  # @user = User.find(session[:user_id])
   erb :home
-  # add 2 homepage for signed in or signed out
 end
 
 get '/sign_in' do 
-  # @user = User.find(session[:user_id])
   erb :sign_in
 end
 
 post '/sign_in' do 
-  # @user = User.find_by(username: params[:username])
   # if user exists and user and password match, start session
   if @user && @user.password == params[:password]
     session[:user_id] = @user.id
@@ -37,7 +32,6 @@ post '/sign_in' do
     redirect '/sign_up'
   end
 end
-
 
 get '/sign_up' do 
   erb :sign_up
@@ -58,12 +52,6 @@ end
 
 get '/create_post' do 
   @user = User.find(session[:user_id])
-  
-  # @user = User.find(params[:username])
-
-# this will grab all posts
-  # @posts = Post.all
-
   erb :create_post
 end
 
@@ -74,22 +62,13 @@ post '/create_post' do
     title: params[:title],
     main_text: params[:main_text]
   )
-
-  
     redirect "/profile"
-  
 end
 
-
-
-
-
 get '/profile' do
-
   @user = User.find(session[:user_id])
   erb :profile
 end
-
 
 get '/sign_out' do
   @sign_out = session[:user_id] = nil
@@ -99,8 +78,7 @@ end
 get '/delete_account' do 
   @user = User.find(session[:user_id]).destroy
   session[:user_id] = nil
-  erb :delete_account
-  
+  erb :delete_account  
 end
 
 get'/browse' do
@@ -112,9 +90,12 @@ post '/browse' do
   @posts = Post.all
 end
 
-
 get '/account' do 
   @user = User.find(session[:user_id])
   erb :account
 end
 
+# get '/profile'
+#   @user = erb: user(params[:id])
+
+# end
